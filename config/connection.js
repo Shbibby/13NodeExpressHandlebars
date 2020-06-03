a6d5ad115e77bd15304effa35aca5a5e97e61b02
Shbibby <joshstillion@gmail.com>
2020-06-03 00:22:10 -0400

    added connection.js code

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,25 @@
 // setup code to connect to MySQL
 
+// setup the code to connect Node to MySQL
+
+const mysql = require("mysql");
+
+let connection;
+connection = mysql.createConnection({
+  host:"localhost",
+  port:"8080",
+  user:"root",
+  password:"1234",
+  database:"burgers_db"
+})
+
+connection.connect(function(error){
+  if(error){
+    console.log("connection.js error - line 16 not connecting")
+  } else {
+    console.log("connection.js connected successfully")
+  }
+})
 // export connection
+  module.exports = connection;
+//
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,4 +2,8 @@ const express = require("express");
 const burger = require("./models/burger.js");
 
 // create router
+  const router = express.Router();
+//
+
 // export router
+  module.exports = router;
